7fd783ee035eb593f41e31cd661991fe25a6f86f
John Morgan <jtmorgan61@gmail.com>
2013-12-16 12:45:20 -0800

    chapter 2

---
+++ chapter2/calc.rb
@@ -0,0 +1,18 @@
+# first calc
+puts 1 + 2
+
+# floats
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# integers
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+# experiments
+puts 5 * (12 - 8) + -15
+puts 98 + (59872 / (13 * 8)) * -51
