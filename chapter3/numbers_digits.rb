446c36f9df31f23af94cde08277e162c5eda6615
John Morgan <jtmorgan61@gmail.com>
2013-12-16 12:53:00 -0800

    chapter 3

---
+++ chapter3/numbers_digits.rb
@@ -0,0 +1,12 @@
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re rockin\' this codin\' exercise!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
