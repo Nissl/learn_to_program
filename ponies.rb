672bb12ab4a39c019d84f03c2c7bbd6fd0fa2f88
John Morgan <jtmorgan61@gmail.com>
2013-12-16 12:20:09 -0800

    update practice

---
+++ ponies.rb
@@ -0,0 +1,9 @@
+# I really like ponies!
+
+# Yes, this is a hilarious joke. 
+
+# Also a way to fill up a chapter 1 folder. Unless I missed something, the 
+# author doesn't have us do anything in that chapter other than setup we've
+# already done, but he mentions this file name as an example.
+
+# I really do like ponies, though.
