1a24f4bdc2d74d16aba41280e48b919c41f47f4c
John Morgan <jtmorgan61@gmail.com>
2013-12-16 12:18:07 -0800

    chapter 1 (no code)

---
+++ chapter1/ponies.rb
@@ -0,0 +1,7 @@
+# I really like ponies!
+
+# Yes, this is a hilarious joke. Also a way to fill up a chapter 1 
+# folder. The author doesn't have us do anything in that chapter
+# but he mentions this file name as an example.
+
+# I really do like ponies, though.
