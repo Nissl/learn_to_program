bdfbd767efe042ac8e875238b85067c9391b1ced
John Morgan <jtmorgan61@gmail.com>
2013-12-16 12:23:57 -0800

    one more test

--- chapter1/ponies.rb
+++ chapter1/ponies.rb
@@ -1,7 +1,12 @@
 # I really like ponies!
 
-# Yes, this is a hilarious joke. Also a way to fill up a chapter 1 
-# folder. The author doesn't have us do anything in that chapter
-# but he mentions this file name as an example.
+# Yes, this is a hilarious joke. 
+
+# Also, it's a way to fill up a chapter 1 folder so my OCD is satisfied. 
+# Unless I missed something, the author doesn't have us do anything in that 
+# chapter other than setup we've already done, but he mentions this file 
+# name as an example.
+
+# Also this lets me test some mucking around with git commands.
 
 # I really do like ponies, though.
